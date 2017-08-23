a74d7f24e584c7bdf62310dba1b68b6506a39905
Romain Dewaele <dewaeleromain@gmail.com>
2017-08-23 08:58:11 +0000

    Updated routes files with new devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
+  devise_for :users, controllers: {registeration: 'users/registeration'}
   resources :contacts
   get '/about' => 'pages#about'
   root 'pages#home'
